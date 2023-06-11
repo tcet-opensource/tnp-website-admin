062694173857a940169fda79044fcff322534e7b
Hitansh Doshi <hkd159@gmail.com>
2023-06-11 23:42:19 +0530

    updated structure

--- src/api/offer/controllers/offer.js
+++ src/api/offer/controllers/offer.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * company controller
+ * offer controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::company.company');
+module.exports = createCoreController('api::offer.offer');
